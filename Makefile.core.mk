bf8b42a7cffc8363ce6c757c04d1a94ac7897c0c
twistio-automation <109822249+twistio-automation@users.noreply.github.com>
2023-10-12 18:32:20 -0600

    Update BASE_VERSION to 1.18-2023-10-12T19-03-01 (#271)
    Co-authored-by: twistio-automation <>

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -38,7 +38,7 @@ SHELL := /bin/bash -o pipefail
 export VERSION ?= 1.18-dev
 
 # Base version of Istio image to use
-BASE_VERSION ?= 1.18-2023-10-06T06-27-12
+BASE_VERSION ?= 1.18-2023-10-12T19-03-01
 ISTIO_BASE_REGISTRY ?= gcr.io/istio-release
 
 export GO111MODULE ?= on
